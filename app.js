cb05a43116f0b72c6141e0548adc48d87bf565fc
maryamaljanabi <maryam.aljanabi135@gmail.com>
2020-05-21 21:20:53 +0300

    Add screenshots

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
-require('dotenv').config();
-var express          = require("express"),
-    app              = express(),
-    bodyParser       = require("body-parser"),
-    mongoose         = require("mongoose"),
-    passport         = require("passport"),
-    LocalStrategy    = require("passport-local"),
-    methodOverride   = require("method-override"),
-    flash            = require("connect-flash"),
-    GeoJSON          = require('geojson'),
-    User             = require("./models/user"),
-    Destination      = require("./models/destination"),
-    Comment          = require("./models/comment.js");
+require("dotenv").config();
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  flash = require("connect-flash"),
+  GeoJSON = require("geojson"),
+  User = require("./models/user"),
+  Destination = require("./models/destination"),
+  Comment = require("./models/comment.js");
 
 var commentRoutes = require("./routes/comments"),
-    destinationRoutes = require("./routes/destinations"),
-    indexRoutes = require("./routes/index");
+  destinationRoutes = require("./routes/destinations"),
+  indexRoutes = require("./routes/index");
 
 var url = process.env.DATABASEURL || "mongodb://localhost/travel_destinations";
 var port = process.env.PORT || 3000;
 mongoose.connect(url);
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: false,
+  })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((err) => {
+    console.log("ERROR: ", err.message);
+  });
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 
 //PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "whatever sentence you may like",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -41,18 +55,17 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //A middleware to pass currentUser to all routes
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 //requiring routes
 app.use(indexRoutes);
 app.use("/destinations/:id/comments", commentRoutes);
 app.use("/destinations", destinationRoutes);
-
-app.listen(port, () => { console.log("Server online..."); });
-
-
+app.listen(port, () => {
+  console.log("Server online on port: ", port);
+});
